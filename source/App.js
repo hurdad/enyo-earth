0cde367479b874848d949d8fdbd3188b193b48c9
Alex <root@servo.localdomain>
2013-04-24 02:56:39 -0400

    init

--- source/App.js
+++ source/App.js
@@ -3,15 +3,127 @@ enyo.kind({
 	kind: "FittableRows",
 	fit: true,
 	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
+		{kind: "onyx.Toolbar", content: "Enyo Earth"},
+		{kind: "FittableColumns", fit: true, components:[
+			{kind: "Accordion", style: "width: 300px;", headerHeight: 40, onViewChange: "viewChanged", onInit: "onInit", components: [
+
+			    // First item of accordion
+			    {kind: "AccordionItem", headerTitle: "Search", maxHeight: 200, contentComponents:[
+			   		{style: "padding:10px;", components:[
+				    	{kind: "onyx.InputDecorator", setAlwaysLooksFocused: true, components: [
+							{name: "input_kml", kind: "onyx.Input", placeholder: "Address or Location"},
+							{kind: "Image", src: "assets/search-input-search.png"}
+						]},
+					]}
+			    ]},
+
+			    // Second item of accordion
+			    {kind: "AccordionItem", headerTitle: "Places", contentComponents:[
+			        {kind: "Scroller", fit: true, components: [
+						{kind: "Node", icon: "assets/folder-open.png", content: "My Places", expandable: true, expanded: true, onExpand: "nodeExpand", onNodeTap: "nodeTap", components: [
+							{icon: "assets/file.png", content: "Alpha"},
+						]},
+					]}
+			    ]},
+
+			    // Second item of accordion
+			    {kind: "AccordionItem", headerTitle: "Layers", contentComponents:[
+			        {name: "info", kind: "Scroller", classes: "enyo-fit", components: [
+					
+						{kind: "onyx.Groupbox", style: "padding: 10px;", classes: "settings", components: [
+							{kind: "onyx.GroupboxHeader", style: "padding: 10px;",content: "Layers"},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Borders"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{ style: "float: right;display: inline-block;", content:"Buildings"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Buildings Low Res"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Roads"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Terrain"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Trees"}
+							]}
+						]},
+						{kind: "onyx.Groupbox", style: "padding: 10px;", classes: "settings", components: [
+							{kind: "onyx.GroupboxHeader", style: "padding: 10px;",content: "Overlays"},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Scale Legend"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Status Bar"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Overview Map"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Grid"}
+							]},
+							{components:[
+								{kind: "onyx.ToggleButton", onChange: "toggleChangedHandler", classes: "labeled-item-icon"},
+								{style: "float: right;display: inline-block;", content:"Atmosphere"}
+							]},
+						]},
+					]}
+			    ]}
+
+			]},
+			{name: "google_earth", kind: "GoogleEarth", fit: true},
 		]},
 		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
+			{kind: "onyx.Button", content: "Add KML", ontap: "addKMLTap"},
+			{kind: "onyx.InputDecorator", components: [
+				{name: "input_kml", kind: "onyx.Input", style: "width:300px", placeholder: "Enter kml url here"}
+			]},
+			{kind: "onyx.Button", content: "Tap", ontap: "ontap"},
+
 		]}
 	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
+	rendered: function() {
+		this.inherited(arguments);
+console.log('test');
+		//var index = 0;
+		//this.$.accordion.toggleItem( this.$.accordion.getItems()[ index ] );
+	},
+	addKMLTap: function(inSender, inEvent) {
+		var href = this.$.input_kml.getValue();
+		//console.log(this.$.google_earth);
+		this.$.google_earth.setOverviewMapVisibility(true);
+	
+		this.$.google_earth.setNavigationVisibilty(this.$.google_earth.getInstance().VISIBILITY_AUTO);
+
+
+		this.$.google_earth.fetchKml('http://75.80.174.85/kml_example.kml');
+
+		//console.log(this.$.google_earth.getInstance().getFeatures().getChildNodes().getLength())
+	},
+	ontap: function(inSender, inEvent) {
+		//	console.log(this.$.google_earth.getInstance().getFeatures().getChildNodes().getLength())
+		//	console.log(this.$.google_earth.getInstance().getFeatures().getChildNodes().item(0).getType());
+		//	console.log(this.$.google_earth.getInstance().getFeatures().getChildNodes().item(0).getFeatures().getChildNodes().item(0).getType());
+
+		//	this.$.my_tree.createComponent(	{icon: "assets/file.png", content: "Alpha2"});
+		//	this.$.my_tree.render();
+
+
+		console.log(this.$.google_earth.getInstance().getElementByUrl('http://75.80.174.85/kml_example.kml#2').getType());
+	
+
 	}
 });
---
+++ source/google-earth.js
@@ -0,0 +1,126 @@
+/**
+ * The maps api is loaded using the Google Loader. Include
+ * <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+ * in your index.html to use this component.
+ */
+enyo.kind({
+	name: 'GoogleEarth',
+	kind: 'Control',
+	published: {
+		apiVersion: '1',
+		otherMapParams: ''
+	},
+	events: {
+		onEarthCreated: ''
+	},
+	constructor: function () {
+		this.inherited(arguments);
+	},
+	components: [{
+			name: 'earth',
+			classes: 'enyo-google-earth'
+		}
+	],
+	//* @protected
+	create: function () {
+		this.inherited(arguments);
+		this.load();
+	},
+	load: function () {
+		google.load('earth', this.apiVersion, {
+			other_params: this.otherMapParams
+		});
+
+		google.setOnLoadCallback(enyo.bind(this, 'init'));
+	},
+	init: function () {
+		if (this.$.earth.hasNode()) 
+      		google.earth.createInstance(this.$.earth.node, enyo.bind(this, 'initCB'), enyo.bind(this, 'failureCB'));
+    },
+    initCB: function(instance) {
+		this.ge = instance;
+		this.ge.getWindow().setVisibility(true);
+		this.doEarthCreated();
+    },
+    failureCB: function(errorCode) {
+    },
+    fetchKmlCB: function(kmlObject){
+   		if (kmlObject)
+          this.ge.getFeatures().appendChild(kmlObject);
+       	if (kmlObject.getAbstractView() !== null)
+          this.ge.getView().setAbstractView(kmlObject.getAbstractView());
+    },
+    //* @public
+    getInstance: function(){
+    	return this.ge;
+    },
+    fetchKml: function(href){
+        google.earth.fetchKml(this.ge, href, enyo.bind(this, 'fetchKmlCB'));
+    },
+    networkLinkKML: function(href, refreshVisibility, flyToView){
+    	var link = ge.createLink('');
+		link.setHref(href);
+		var networkLink = ge.createNetworkLink('');
+		networkLink.set(link, refreshVisibility, flyToView); // Sets the link, refreshVisibility, and flyToView
+		ge.getFeatures().appendChild(networkLink);
+    },
+    toggleLayers: function(layer_name, visiblity){
+		this.ge.getLayerRoot().enableLayerById(layer_name, visiblity);
+    },
+    setNavigationVisibilty: function(visiblity){
+    	this.ge.getNavigationControl().setVisibility(visiblity);
+    },
+    setNavigationPosition: function(x_units, y_units){
+    	this.ge.getNavigationControl().getScreenXY().setXUnits(x_units);
+  		this.ge.getNavigationControl().getScreenXY().setYUnits(y_units);
+    },
+    setScaleLegendVisibilty: function(visiblity){
+		this.ge.getOptions().setScaleLegendVisibility(visiblity);
+    },
+    setStatusBarVisibility: function(visiblity){
+    	this.ge.getOptions().setStatusBarVisibility(visiblity);
+    },
+    setOverviewMapVisibility: function(visiblity){
+    	this.ge.getOptions().setOverviewMapVisibility(visiblity);
+    },
+    setGridVisibility: function(visiblity){
+    	this.ge.getOptions().setGridVisibility(visiblity);
+    },
+    setAtmosphereVisibility: function(visiblity){
+    	this.ge.getOptions().setAtmosphereVisibility(visiblity);
+    },
+    setTerrainExaggeration: function(value){
+    	this.ge.getOptions().setTerrainExaggeration(value);
+	},
+	setFlyToSpeed: function(value){
+		this.ge.getOptions().setFlyToSpeed(value);
+	},
+	setFadeInOutEnabled: function(value){
+		this.ge.getOptions().setFadeInOutEnabled(value);
+	},
+	panCamera: function(latitude, longitude, range){
+		var lookAt = this.ge.createLookAt('');
+		lookAt.setLatitude(latitude);
+		lookAt.setLongitude(longitude);
+		lookAt.setRange(range); 
+		this.ge.getView().setAbstractView(lookAt);
+	},
+	addPlacemark: function(name, latitude, longitude){
+		var placemark = this.ge.createPlacemark(name);  
+		var point = this.ge.createPoint('');
+		point.setLatitude(latitude);
+		point.setLongitude(longitude);
+		placemark.setGeometry(point);
+		this.ge.getFeatures().appendChild(placemark);
+	},
+	addGroundOverlay: function(href, north, south, east, west, rotation){
+		var groundOverlay = ge.createGroundOverlay('');
+		var icon = ge.createIcon('');
+		icon.setHref(href);
+		groundOverlay.setIcon(icon);
+		var latLonBox = ge.createLatLonBox('');
+		latLonBox.setBox(north, south, east, west, rotation);
+		groundOverlay.setLatLonBox(latLonBox);
+		ge.getFeatures().appendChild(groundOverlay);
+	}
+});
